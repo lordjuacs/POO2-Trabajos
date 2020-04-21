834483611f85a3fcc863c9d4eb3199330f48efae
Joaquin Ramirez <juacsr@gmail.com>
2020-04-20 20:43:10 -0500

    F

--- Semana_2/ejercicios/funciones.h
+++ Semana_2/ejercicios/funciones.h
@@ -2,6 +2,10 @@
 // Created by joaquin_ramirez on 4/20/20.
 //
 #include <iostream>
+#include <vector>
+#include <list>
+#include <cassert>
+#include <ctime>
 using namespace std;
 #ifndef EJERCICIOS_FUNCIONES_H
 #define EJERCICIOS_FUNCIONES_H
@@ -14,17 +18,65 @@ Iterator find(Iterator start, Iterator end, const Object& x){
             return it;
     return end;
 }
+
 //ej2
 template <typename InputIt1, typename InputIt2, typename OutputIt>
 OutputIt merge(InputIt1 first1, InputIt1 last1, InputIt2 first2, InputIt2 last2, OutputIt d_first){
     while (true) {
         if (first1==last1)
             return copy(first2,last2,d_first);
-        if (first2==last2)
-            return copy(first1,last1,d_first);
         *d_first++ = *first1++;
     }
 }
+
+//ej3
+template <typename InputIt1, typename InputIt2, typename OutputIt>
+OutputIt intersection(InputIt1 first1, InputIt1 last1, InputIt2 first2, InputIt2 last2, OutputIt d_first){
+    while (first1!=last1 and first2!=last2)
+    {
+        if (*first1<*first2)
+            first1++;
+        else if (*first2<*first1)
+            first2++;
+        else {
+            *d_first++ = *first1++;
+            first2++;
+        }
+    }
+    return d_first;
+}
+//ej4
+template <typename InputIt1, typename InputIt2, typename OutputIt>
+OutputIt united(InputIt1 first1, InputIt1 last1, InputIt2 first2, InputIt2 last2, OutputIt d_first){
+    while(first1 != last1 and first2 != last2) {
+            if (*first1 < *first2) {
+                *d_first++ = *first1++;
+            }
+            else if (*first2 < *first1) {
+                *d_first++ = *first2++;
+            }
+            else {
+                *d_first++ = *first1++;
+                ++first2;
+
+            }
+            if(first1 == last1){
+                while(first2 != last2)
+                    *d_first++ = *first2++;
+            }
+            else if(first2 == last2){
+                while(first1 != last1)
+                    *d_first++ = *first1++;
+            }
+    }
+    return d_first;
+}
+
+//ej5
+template< typename type, template<typename ...> class Container>
+void insert_sorted(Container<type> cont, type value){
+}
+
 
 
 #endif //EJERCICIOS_FUNCIONES_H
--- Semana_2/ejercicios/main.cpp
+++ Semana_2/ejercicios/main.cpp
@@ -1,5 +1,5 @@
 #include "funciones.h"
-#include <vector>
+
 int main() {
     //ej1
     vector<double> v1 = {1,2,3};
@@ -7,18 +7,40 @@ int main() {
     auto fin = cend(v1);
     auto elemento_si = find(ini, fin, 2);
     auto elemento_no = find(ini, fin, 4);
-
+    assert(elemento_si != fin);
+    assert(elemento_no == fin);
 
     //ej2
     int first[] = {1,2,3,4,5};
     int second[] = {1,2,3,4,5};
     vector<int>v2(10);
     merge(first, first + 5, second, second + 5, v2.begin());
-    cout << "Elementos nuevo vector:" << endl;
+    cout << "Ej 2:Elementos nuevo vector:" << endl;
     for(auto element:v2)
         cout << element << endl;
+    cout << endl;
 
     //ej3
+    list<int>list1 = {2,24,76};
+    list<int>list2 = {1,2,24,25};
+    list<int>list3(2);
+    intersection(list1.begin(), list1.end(),list2.begin(), list2.end(), list3.begin());
+    cout << "Ej 3: Elementos  lista interseccion" << endl;
+    for(auto& elem:list3)
+        cout << elem << endl;
+    cout << endl;
+
+    //ej4
+    list<int>list21 = {1,2,4,7};
+    list<int>list22 = {3,4,5};
+    list<int>list23(6);
+    united(list21.begin(), list21.end(),list22.begin(), list22.end(), list23.begin());
+    cout << "Ej 4: Elementos  lista union" << endl;
+    for(auto& elem:list23)
+        cout << elem << endl;
+    cout << endl;
+
+
 
     return 0;
 }
